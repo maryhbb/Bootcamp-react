dc86a5c960cd0408d24b87811c0e55fce057879f
Mary <76174247+maryhbb@users.noreply.github.com>
2023-08-15 22:31:22 +0200

    init react-basics/attributes project

--- react-basics_hello-world-article/src/App.js
+++ react-basics_hello-world-article/src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return <HelloWorldArticle />;
+}
+
+export function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>Welcome to My First React Component</h1>
+      <p>
+        Embark on a journey of coding with me as I delve into the world of React
+        components. In this exciting venture, we will explore the building blocks
+        of modern web development and harness the power of React to create
+        dynamic and interactive user interfaces.
+      </p>
+    </article>
+  );
 }

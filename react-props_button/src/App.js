b81aa6f546d0f542af741716153589c84df52440
Mary <76174247+maryhbb@users.noreply.github.com>
2023-08-18 18:40:15 +0200

    Implement react-props/button task

--- react-props_button/src/App.js
+++ react-props_button/src/App.js
@@ -2,5 +2,55 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  function handleClick() {
+    alert("You clicked Button for Task4!");
+  }
+  return (
+    <>
+      <div>
+        <Button
+          disabled={false}
+          text="Adding a Click Event"
+          style={{
+            backgroundColor: "red",
+            color: "white",
+            border: "none",
+            margin: "20px",
+            padding: "10px 20px",
+            borderRadius: "5px",
+            cursor: "pointer",
+          }}
+          onClick={() => alert("You clicked Button for Task 2!")}
+        />
+      </div>
+      <div>
+        <Button
+          disabled={false}
+          text="Pass a Function as Prop"
+          style={{
+            backgroundColor: "blue",
+            color: "white",
+            border: "none",
+            margin: "20px",
+            padding: "10px 20px",
+            borderRadius: "5px",
+            cursor: "pointer",
+          }}
+          onClick={handleClick}
+        />
+      </div>
+    </>
+  );
+}
+
+function Button({ color, style, disabled, text, onClick }) {
+  return (
+    <button
+      disabled={disabled}
+      style={{ color: color, ...style }}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
 }

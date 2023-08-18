b48dfb8ef3b7e0c5e928ae3efb358b734d6799de
Mary <76174247+maryhbb@users.noreply.github.com>
2023-08-18 10:27:46 +0200

    Implement react-props/greeting task

--- react-props_greeting/src/App.js
+++ react-props_greeting/src/App.js
@@ -1,5 +1,10 @@
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Greeting name="Mary" />;
+}
+
+let coachName;
+function Greeting(props) {
+  return <h1>Hello {coachName ? "Coach" : props.name}!</h1>;
 }

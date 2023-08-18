48306e1443d9a5a28fafa2ce26498a3b5e290ec3
Mary <76174247+maryhbb@users.noreply.github.com>
2023-08-18 18:39:22 +0200

    Implement react-props/smiley task

--- react-props_smiley/src/App.js
+++ react-props_smiley/src/App.js
@@ -2,5 +2,9 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Smiley isHappy />;
+}
+
+function Smiley({ isHappy }) {
+  return <h1>{isHappy ? "Feeling Happy 😌" : "Feeling Sad 😢"}</h1>;
 }

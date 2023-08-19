8441e8e9ba905c609b1879c1450b206dafeb25fa
Mary <76174247+maryhbb@users.noreply.github.com>
2023-08-19 23:21:22 +0200

    Implement 1. Header component

--- react-nesting_navigation/src/App.js
+++ react-nesting_navigation/src/App.js
@@ -1,34 +1,12 @@
 import "./styles.css";
-
 import avatar from "./img/avatar.jpg";
 import logo from "./img/logo.jpg";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <>
-      <header className="header">
-        <a href="#">
-          <img className="round-image" src={logo} alt="logo" />
-        </a>
-        <nav>
-          <a className="navigation__link" href="#home">
-            Home
-          </a>
-          <a className="navigation__link" href="#about">
-            About
-          </a>
-          <a className="navigation__link" href="#impressum">
-            Impressum
-          </a>
-        </nav>
-        <button
-          type="button"
-          onClick={() => console.log("I could toggle a profile!")}
-          aria-label="toggle profile"
-        >
-          <img className="round-image" src={avatar} alt="avatar" />
-        </button>
-      </header>
+      <Header></Header>
       <main>content goes here…</main>
     </>
   );

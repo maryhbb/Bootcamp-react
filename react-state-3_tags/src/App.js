b963bdfbc41ebfd1cfaf10805285e47eb516b00c
Mary <76174247+maryhbb@users.noreply.github.com>
2023-08-25 11:09:18 +0200

    Implement react-state-3/tags task

--- react-state-3_tags/src/App.js
+++ react-state-3_tags/src/App.js
@@ -6,10 +6,18 @@ import List from "./components/List/index.js";
 export default function App() {
   const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
 
+  const handelAddTag = (newTag) => {
+    setTags([...tags, newTag]);
+  };
+
+  const handelDeleteTag = (tagToDelete) => {
+    setTags(tags.filter((tag) => tag !== tagToDelete));
+  };
+
   return (
     <main className="app">
-      <Form />
-      <List tags={tags} />
+      <Form onAddTag={handelAddTag} />
+      <List tags={tags} onDeleteTag={handelDeleteTag} />
     </main>
   );
 }

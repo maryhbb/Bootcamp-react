ccfb57f387f51e519ac324140c2294dae9219f53
Mary <76174247+maryhbb@users.noreply.github.com>
2023-08-28 14:41:56 +0200

    Implement react-effects-and-fetch/iss-tracker task

--- react-effects-and-fetch_iss-tracker/src/App.js
+++ react-effects-and-fetch_iss-tracker/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Controls from "./components/Controls";
 import Map from "./components/Map";
 import "./styles.css";
@@ -11,7 +11,27 @@ export default function App() {
     latitude: 0,
   });
 
-  async function getISSCoords() {}
+  async function getISSCoords() {
+    try {
+      const response = await fetch(URL);
+      if (response.ok) {
+        const data = await response.json();
+        setCoords({ longitude: data.longitude, latitude: data.latitude });
+      } else {
+        console.error("Failed to fetch ISS coordinates");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    const timer = setInterval(getISSCoords, 5000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
 
   return (
     <main>
